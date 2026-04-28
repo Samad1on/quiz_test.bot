016ef07d7c10612cab9b63fea276f0709adc57c7
samadjon <samadjonabdullayev50@gmail.com>
2026-04-29 00:27:46 +0500

    tayyor savollar qoshlmoqda

--- src/quiz.js
+++ src/quiz.js
@@ -1,8 +1,22 @@
+// quiz.js
+
 export const questions = [
   {
     id: 1,
     question: "Ot nima?",
     answers: ["So‘z turi", "Gap", "Tovush", "Raqam"],
+    correct: 0,
+  },
+  {
+    id: 2,
+    question: "Fe’l nima?",
+    answers: ["Harakat", "Predmet", "Rang", "Son"],
+    correct: 0,
+  },
+  {
+    id: 3,
+    question: "Sifat nima?",
+    answers: ["Belgi", "Harakat", "Raqam", "Joy"],
     correct: 0,
   },
 ];
