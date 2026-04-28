ba2b7e8f5bf09d190687e2b82dd5d7d21565dc42
samadjon <samadjonabdullayev50@gmail.com>
2026-04-29 00:20:20 +0500

    tayyor savollar qoshlmoqda

--- src/quiz.js
+++ src/quiz.js
@@ -1,16 +1,8 @@
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-import { parseQuestions } from "./parser.js";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// to‘liq path (serverga 100% mos)
-const filePath = path.join(__dirname, "../data/questions.txt");
-
-const text = fs.readFileSync(filePath, "utf-8");
-
-export const questions = parseQuestions(text);
-export const users = {};
-export const timers = {};
+export const questions = [
+  {
+    id: 1,
+    question: "Ot nima?",
+    answers: ["So‘z turi", "Gap", "Tovush", "Raqam"],
+    correct: 0,
+  },
+];
