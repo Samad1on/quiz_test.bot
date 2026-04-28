92dea22770dd657c7f049b080c1c64aa31b14c3b
samadjon <samadjonabdullayev50@gmail.com>
2026-04-29 00:13:41 +0500

    tayyor savollar qoshlmoqda

--- src/quiz.js
+++ src/quiz.js
@@ -1,7 +1,15 @@
 import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 import { parseQuestions } from "./parser.js";
 
-const text = fs.readFileSync("./data/questions.txt", "utf-8");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, "../data/questions.txt");
+
+const text = fs.readFileSync(filePath, "utf-8");
+
 export const questions = parseQuestions(text);
 
 export const users = {};
